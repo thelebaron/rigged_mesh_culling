02dfbfb8911531c4bb754eb6a56d0e2e386d0e70
thelebaron <thelebaron@gmail.com>
2020-04-16 13:58:49 -0400

    -

--- Assets/CharacterRigPostProcessor.cs
+++ Assets/CharacterRigPostProcessor.cs
@@ -1,12 +1,14 @@
-﻿using UnityEditor;
+﻿
 using UnityEngine;
-
+#if UNITY_EDITOR
+using UnityEditor;
 public class CharacterRigPostProcessor : AssetPostprocessor
 {
-    
     void OnPreprocessModel()
     {
-        ModelImporter modelImporter = assetImporter as ModelImporter;
+        var modelImporter = assetImporter as ModelImporter;
+        if (modelImporter == null) 
+            return;
         modelImporter.isReadable = true;
         //modelImporter.materialName = ModelImporterMaterialName.BasedOnMaterialName;
         //modelImporter.materialSearch = ModelImporterMaterialSearch.Everywhere;
@@ -29,7 +31,7 @@ public class CharacterRigPostProcessor : AssetPostprocessor
             var mesh = skinnedMeshRenderer.sharedMesh;
             
             // https://forum.unity.com/threads/preskinned-bindpose-vertex-position.861220/#post-5697853
-            // set TEXCOORD3 to the object's vertex positions, thanks mr snail!
+            // set TEXCOORD3 to the object's vertex positions, thanks mr bgolus!
             mesh.SetUVs(3, mesh.vertices);
 
             skinnedMeshRenderer.sharedMesh = mesh;
@@ -46,3 +48,4 @@ public class CharacterRigPostProcessor : AssetPostprocessor
             Apply(child);
     }
 }
+#endif
